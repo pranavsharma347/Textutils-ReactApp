8583060b8fcea87bfe13e21a407f298a34790b64
pranavsharma347 <67367475+pranavsharma347@users.noreply.github.com>
2022-12-12 09:15:28 +0530

    Updates

---
+++ precache-manifest.0e0881267ff8773ec0241aad6622d0c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b8dc187cf74b45b2924",
+    "url": "/Textutils-ReactApp/static/css/main.91ef382d.chunk.css"
+  },
+  {
+    "revision": "1b8dc187cf74b45b2924",
+    "url": "/Textutils-ReactApp/static/js/main.1b8dc187.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/Textutils-ReactApp/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/Textutils-ReactApp/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "5cac324133433f99ec62",
+    "url": "/Textutils-ReactApp/static/js/runtime~main.5cac3241.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Textutils-ReactApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1a55562178a3145680abb923b1c72955",
+    "url": "/Textutils-ReactApp/index.html"
+  }
+];
